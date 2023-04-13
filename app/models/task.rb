588a1f355d652ddad5db138efa5e9b1a8328a3a5
Wojciech <wojciech.dabrovski@gmail.com>
2023-04-14 00:37:26 +0200

    Add deadline field to tasks (#3)
    * Add migration file
    
    * Add deadline field and update tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+  validates :deadline, presence: true
+
+  scope :by_deadline, -> { order(deadline: :asc) }
+
+  def formatted_deadline
+    deadline.strftime("%d-%m-%Y")
+  end
 end
